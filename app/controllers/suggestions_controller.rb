ad143ff624942da221514825022716ef287cad5d
Code-Vortex <darkbrotherhood.assassin@gmail.com>
2014-03-07 01:42:09 +0200

    modifying

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -3,18 +3,27 @@ class SuggestionsController < ApplicationController
 	before_filter :set_user, only: [:new, :create]
 	
 	def index
-		@suggestions = Suggestion.all
-	end
+    if params[:tab] == 'all_suggestions'
+      @suggestions = Suggestion.all
+    elsif params[:tab] == 'theoretical'
+      @suggestions = Suggestion.theoretical
+    elsif params[:tab] == 'practical'
+      @suggestions = Suggestion.practical
+    elsif params[:tab] == 'projects'
+      @suggestions = Suggestion.project
+    end
 
-	def show
-		@suggestion = Suggestion.find(params[:id])
+  end
+
+  def show
+    @suggestion = Suggestion.find(params[:id])
     @reply = Reply.new
-	end
+  end
 
 
-	def new
-		@suggestion = Suggestion.new
-		respond_to do |format|
+  def new
+    @suggestion = Suggestion.new
+    respond_to do |format|
       		format.html # new.html.erb
       		format.js # new.js.erb
       		format.json { render json: @suggestion }
@@ -22,12 +31,12 @@ class SuggestionsController < ApplicationController
       end
 
       def create
-            @suggestion = Suggestion.new(params[:suggestion])
-            if @suggestion.save
-                  redirect_to  subjects_path(tab: "enrolled"), notice: "Suggestion Created."
-            else
-                  redirect_to  subjects_path(tab: "enrolled"), notice: "Something went wrong."
-            end
+        @suggestion = Suggestion.new(params[:suggestion])
+        if @suggestion.save
+          redirect_to  subjects_path(tab: "enrolled"), notice: "Suggestion Created."
+        else
+          redirect_to  subjects_path(tab: "enrolled"), notice: "Something went wrong."
+        end
       end
 
       def vote_up
@@ -35,14 +44,14 @@ class SuggestionsController < ApplicationController
 
       private
       def set_subject
-            @title = Subject.find(params[:subject_id]).subject_title
-            @subject = Subject.find(params[:subject_id]).id
+        @title = Subject.find(params[:subject_id]).subject_title
+        @subject = Subject.find(params[:subject_id]).id
       end
 
       def set_user
-           if current_user.student
-                @student = current_user.student.id
-                @name  = Student.find(@student)
-          end
+       if current_user.student
+        @student = current_user.student.id
+        @name  = Student.find(@student)
+      end
     end
-end
+  end
--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -1,9 +1,12 @@
 class Suggestion < ActiveRecord::Base
-  attr_accessible :subject_field_id, :content, :student_id, :subject_id, :title, :replies_attributes
-  belongs_to :subject_field
-  belongs_to :student
-  belongs_to :subject
-  has_many :votes
-  has_many :replies
-  accepts_nested_attributes_for :replies
+	scope :theoretical, joins(:subject_field).where("field_name = ?", 'Theoretical')
+	scope :practical , joins(:subject_field).where("field_name = ?", 'Practical')
+	scope :project , joins(:subject_field).where("field_name = ?", 'Project')
+	attr_accessible :subject_field_id, :content, :student_id, :subject_id, :title, :replies_attributes
+	belongs_to :subject_field
+	belongs_to :student
+	belongs_to :subject
+	has_many :votes
+	has_many :replies
+	accepts_nested_attributes_for :replies
 end
